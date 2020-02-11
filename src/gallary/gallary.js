1ac50269c56f56b9897ed9fb89b0f5eea55564fc
pandaomeng <daomeng.pan@datayes.com>
2020-02-11 22:39:37 +0800

    feat: 色图列表

--- src/gallary/gallary.js
+++ src/gallary/gallary.js
@@ -1,51 +1,65 @@
-import React, { Component } from 'react';
-import Axios from 'axios';
+import React, { Component } from "react";
+import Axios from "axios";
+import Single from "../singlepicture/single";
 
 export default class Gallary extends Component {
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-                yusa: 1
-        };
-    }
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      yusa: 1,
+      currentPicture: {}
+    };
+  }
 
-    componentDidMount() {
-        Axios('api/getcount.php')
-        .then((response) => {
-            this.setState({yusa: response.data.name});
-        })
-        .catch((error) => {
-            alert(error);
-        });
+  componentDidMount() {
+    // Axios("api/getcount.php")
+    //   .then(response => {
+    //     this.setState({ yusa: response.data.name });
+    //   })
+    //   .catch(error => {
+    //     alert(error);
+    //   });
 
-    }
+    this.setState({
+      pictureList: [
+        {
+          picture_dir: "../favicon.png"
+        },
+        {
+          picture_dir: require("../404.png")
+        }
+      ]
+    });
+  }
 
-    render() {
+  handleClick = item => {
+    console.log("item :", item);
+    this.setState({
+      currentPicture: item
+    });
+  };
+
+  render() {
+    const { currentPicture, pictureList = [] } = this.state;
+    console.log("currentPicture", currentPicture);
+
     return (
-        <div id="gallary" className="d-flex flex-wrap justify-content-around">
-            <div>
-                <img className="card" src={require("./../404.png")}  data-toggle="modal" data-target="#single" alt="1"/>
-                <span className="tag">{this.state.yusa}</span>
-            </div>
-            <div>
-                <img className="card" src={require("./../404.png")}  data-toggle="modal" data-target="#single" alt=""/>
-                <div className="tag">5星</div>
-            </div>
-            <div>
-                <img className="card" src={require("./../404.png")}  data-toggle="modal" data-target="#single" alt=""/>
-                <div className="tag">5星</div>
-            </div>
-            <div>
-                <img className="card" src={require("./../404.png")}  data-toggle="modal" data-target="#single" alt=""/>
-                <div className="tag">5星</div>
-            </div>
-            <div>
-                <img className="card" src={require("./../404.png")}  data-toggle="modal" data-target="#single" alt=""/>
-                <div className="tag">5星</div>
-            </div>
-        </div>
+      <div id="gallary" className="d-flex flex-wrap justify-content-around">
+        {pictureList.map(item => (
+          <div onClick={() => this.handleClick(item)}>
+            <img
+              className="card"
+              src={item.picture_dir}
+              data-toggle="modal"
+              data-target="#single"
+              alt="1"
+            />
+            <span className="tag">{this.state.yusa}</span>
+          </div>
+        ))}
+
+        <Single picture={currentPicture} />
+      </div>
     );
-    }
+  }
 }
