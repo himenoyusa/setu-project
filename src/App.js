69fe764318cf52766e3695146cb463ff193ae92d
shionjiyusa <876203144@qq.com>
2020-02-16 23:25:44 +0800

    add/music player

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'antd/dist/antd.css';
 import Gallery from './gallery/Gallery';
 import PictureBox from './pictureBox/PictureBox';
 import LoginBar from './loginBar/LoginBar';
+import Footer from './footer/Footer';
 
 export default function App() {
   return (
@@ -10,16 +11,7 @@ export default function App() {
       <LoginBar />
       <PictureBox />
       <Gallery />
-
-      <footer>
-        <div>
-          <a href="https://kanata.moe/">访问主页</a>
-        </div>
-        <div>
-          <a href="mail:876203144@qq.com">联系邮箱</a>
-        </div>
-        <div>It&apos;s the only NEET thing to do</div>
-      </footer>
+      {Footer()}
     </>
   );
 }
